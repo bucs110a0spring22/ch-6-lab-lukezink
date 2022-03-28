a4229b81d4be120e78db8ddaa9aa105cf6646733
Luke Zink <83885670+lukezink@users.noreply.github.com>
2022-03-28 03:58:57 +0000

    finished lab

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 
 def seq3np1(n):
     """
@@ -5,14 +6,44 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
+      count+=1
+      if(n % 2) == 0:        # n is even
+        n = n // 2
+      else:                 # n is odd
+        n = n * 3 + 1
+    return count
+
+def lineGraph(uplim):
+  screen = turtle.Screen()
+  ymax = 0
+  for count in range(uplim):
+    ynew = seq3np1(count+1)
+    if ynew > ymax:
+      ymax = ynew
+  screen.setworldcoordinates(0,0,uplim + 10, ymax + 2)
+  screen.bgcolor("White")
+  myturtle = turtle.Turtle()
+  myturtle.speed(0)
+  myturtle.fillcolor("Red")
+  max_so_far = 0
+  for start in range (uplim):
+    ycoor = seq3np1(start+1)
+    if max_so_far < ycoor:
+      max_so_far = ycoor
+    myturtle.goto(start+1,ycoor)
+    result = "Maximum so far: " + str(start+1) + ", " + str(max_so_far)
+    print(result)
+  screen.exitonclick()
 
 def main():
-	seq3np1(3)
+  uplim = int(input("Please enter the upper bound: "))
+  lineGraph(uplim)
+  if (uplim>0):
+    for start in range (uplim):
+      value = int(seq3np1(start+1))
+      print("The start value is " + str(start+1) + ", and the number of iterations is " + str(value))
+  else:
+    print("Invalid value")
 main()
